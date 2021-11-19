a2dd7b60541edaf536019d8ea7feb40a1a9c962e
Nguyen Huu Thang <cambayst@gmail.com>
2021-11-19 13:03:49 +0700

    Updates

---
+++ precache-manifest.d80d781cff6873165c06ae2852f175e0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71f0e9dfd9852c097f028eba05ce090",
+    "url": "/framework-IS2201.M11/index.html"
+  },
+  {
+    "revision": "19175a48ceecdd3a4314",
+    "url": "/framework-IS2201.M11/static/css/2.b2de90d5.chunk.css"
+  },
+  {
+    "revision": "72a5f3238c2f6dde864d",
+    "url": "/framework-IS2201.M11/static/css/main.3707d341.chunk.css"
+  },
+  {
+    "revision": "19175a48ceecdd3a4314",
+    "url": "/framework-IS2201.M11/static/js/2.666a1c6e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/framework-IS2201.M11/static/js/2.666a1c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a5f3238c2f6dde864d",
+    "url": "/framework-IS2201.M11/static/js/main.72484978.chunk.js"
+  },
+  {
+    "revision": "fd0fe0549a4959ed96ba",
+    "url": "/framework-IS2201.M11/static/js/runtime-main.f9bef6d7.js"
+  },
+  {
+    "revision": "0c37f441f2d4a4f68fdc8e5f316a5d43",
+    "url": "/framework-IS2201.M11/static/media/background.0c37f441.jpg"
+  },
+  {
+    "revision": "d48a8973a98231204ed9be58c6095785",
+    "url": "/framework-IS2201.M11/static/media/user.d48a8973.png"
+  }
+]);
